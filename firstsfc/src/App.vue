9867ecc4d89d151122f39f807a04cc59083f2ede
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-01-24 15:01:30 +0800

    FoodItem.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -4,4 +4,23 @@
   <h1>Hello WEBPROG SF231 World!</h1>
 </template>
 
-<style></style>
+<style>
+
+#app > div {
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightgreen;
+  cursor: pointer;
+  
+}
+</style>
+
+
+
+
+
+
+
+
