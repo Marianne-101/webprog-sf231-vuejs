d08ea20f5d67a0025974728e040324d799132353
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-01-24 15:01:30 +0800

    FoodItem.vue

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import FoodItem from './components/FoodItem.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
