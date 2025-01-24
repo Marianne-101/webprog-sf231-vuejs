2d3f680c5c19a52733ae8ff1d7b9aa07040b3e80
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-01-24 15:09:54 +0800

    app.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,20 +1,20 @@
 <script></script>
 
 <template>
-  <h1>Hello WEBPROG SF231 World!</h1>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
 <style>
-
-#app > div {
-  border: dashed black 1px;
-  display: inline-block;
-  margin: 10px;
-  padding: 10px;
-  background-color: lightgreen;
-  cursor: pointer;
-  
-}
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
 </style>
 
 
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 
 const app = createApp(App)
+app.component('food-item', FoodItem)
 app.mount('#app')
