0dd0dbe867cb4c5f6af068cce52d8d54b6c14942
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-01-30 07:06:53 +0000

    Query data from the app

---
+++ firstsfc/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://ifidmdesqwnluhmtagxw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmaWRtZGVzcXdubHVobXRhZ3h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyMTc3ODEsImV4cCI6MjA1Mzc5Mzc4MX0.JyFcsCJhtTBY-7klwXGn6xoOK_uIhMnDT3ABGfTxF4k')
