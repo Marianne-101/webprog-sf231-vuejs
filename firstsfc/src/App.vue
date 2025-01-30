1165944fe520da5a2aaf57d2b388a86f7bc4789c
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-01-30 07:20:23 +0000

    query

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -19,19 +19,12 @@
     <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
   </ul>
 
-  <h1>Food</h1>
-  <personal-profile/>  <br>
-  <food-item/>  <br>
-  <food-item2/> <br>
-  <food-item/>  <br>
-  <food-item2/> <br>
+   <h1>Food</h1>
+  <personal-profile/> <br>
+  <food-item/> 
+  <food-item2/>
+  <food-item/> 
+  <food-item2/> 
 </template>
 
 <style></style>
-
-
-
-
-
-
-
