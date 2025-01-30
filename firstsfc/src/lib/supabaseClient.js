955773e21eb12c541540ce1c7e1d6bf8277fe7c6
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-01-30 07:20:23 +0000

    query

--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://ifidmdesqwnluhmtagxw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmaWRtZGVzcXdubHVobXRhZ3h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyMTc3ODEsImV4cCI6MjA1Mzc5Mzc4MX0.JyFcsCJhtTBY-7klwXGn6xoOK_uIhMnDT3ABGfTxF4k')
+export const supabase = createClient('https://yhulsgxolokeojfsqgkq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodWxzZ3hvbG9rZW9qZnNxZ2txIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyMTc3MzYsImV4cCI6MjA1Mzc5MzczNn0.8kDdlyhwPKIQwaaV-fUc9skTvL9L7YeIW-pQu0B2NXc')
