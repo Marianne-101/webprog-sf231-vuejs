f9a46a681d638bcceea4712f0382239fba5c19da
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-02-03 15:28:37 +0800

    Update FoodItem.vue

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="apple">
+    <div>
       <h2>{{ name }}</h2>
       <p>{{ message }}</p>
     </div>
@@ -10,18 +10,10 @@
     data() {
       return {
         name: 'Apples',
-        message: 'I like apples'
+        message: 'I like apples and oranges'
       }
     }
   };
   </script>
   
-  <style>
-    .apple {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-  </style>
+  <style></style>
