7e1882b8eb077c2165dfcf7633b6b88054c1e74e
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-02-04 06:17:27 +0000

    supabaseClient.ja

--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,7 +1,5 @@
-// lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://ifidmdesqwnluhmtagxw.supabase.co', // Use your actual Supabase URL
-  'your-anon-key' // Use your actual public anon key
+  'https://ifidmdesqwnluhmtagxw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmaWRtZGVzcXdubHVobXRhZ3h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyMTc3ODEsImV4cCI6MjA1Mzc5Mzc4MX0.JyFcsCJhtTBY-7klwXGn6xoOK_uIhMnDT3ABGfTxF4k'
 );
